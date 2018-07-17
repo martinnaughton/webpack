f67f5fb331f1d9a9003287ecd1d5fd44ad847e23
martinnaughton <martin.oneachtain@gmail.com>
2018-07-17 21:15:51 +0100

    added skeleton component

---
+++ template/src/components/Skeleton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skeleton',
+  data () {
+    return {
+      title: 'Skeletone'
+    };
+  },
+  methods: {
+  },
+  computed: {
+  },
+  watch: {
+  },
+  components: {
+  },
+  mounted: function () {
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
