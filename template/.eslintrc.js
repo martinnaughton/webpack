c297d7caa02ef62b3f9ef61cf9850438f98e7438
martinnaughton <martin.oneachtain@gmail.com>
2018-07-17 20:01:47 +0100

    Added semi colon check and update main.js to use plugins

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -69,6 +69,8 @@ module.exports = {
     }],
     {{/if_eq}}
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // enforce semicolons
+   'semi': ['error', 'always']
   }
 }
--- template/src/main.js
+++ template/src/main.js
@@ -2,13 +2,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 {{/if_eq}}
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
 {{#router}}
-import router from './router'
+import router from './router';
 {{/router}}
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(VeeValidate, { errorBagName: 'vErrors', fieldsBagName: 'vFields' });
 
 /* eslint-disable no-new */
 new Vue({
@@ -23,4 +29,4 @@ new Vue({
   components: { App },
   template: '<App/>'
   {{/if_eq}}
-})
+});
