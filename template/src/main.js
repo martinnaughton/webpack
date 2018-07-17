fccebee9fef7cfbe18e13a71af35c0fd6429efd2
martinnaughton <martin.oneachtain@gmail.com>
2018-07-17 21:06:07 +0100

    fixed vee validate

--- template/src/main.js
+++ template/src/main.js
@@ -10,6 +10,7 @@ import router from './router';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import VeeValidate from 'vee-validate';
 
 Vue.config.productionTip = false;
 
